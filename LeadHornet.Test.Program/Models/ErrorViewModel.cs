f2609f11a4857aae91609d5dfbbda50669ad275a
ryanjayson <ryanjayson.berdelao@gmail.com>
2020-07-21 10:47:01 +0800

    upload files

---
+++ LeadHornet.Test.Program/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LeadHornet.Test.Program.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
